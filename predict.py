11846a26730fd4edb1cd25a597a91686b24591cb
ryanglambert <ryan.g.lambert@gmail.com>
2016-02-19 23:18:48 -0800

    added some docstrings

--- predict.py
+++ predict.py
@@ -8,6 +8,12 @@ def load_prod_model(pickle_model_path):
         return pickle.load(pickle_model)
 
 def predict(web_form_inputs):
+    """Takes a list of 7 inputs and outputs a binary predictor from the 
+    loaded model
+    
+    >>>predict.predict([67, 1, 108, 1, 1, 1, 84])
+    Out[20]: [1]
+    """
     model = load_prod_model('./prod_model/prod_model.pkl')
     # print web_form_inputs
     return model.predict(web_form_inputs)
